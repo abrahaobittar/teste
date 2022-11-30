c949d0189525b9bfff4014588450d651d28485d3
Abrahao <abrahaobittar@gmail.com>
2022-11-30 17:31:50 -0300

    adasdasdasdsadsadada

--- teste.php
+++ teste.php
@@ -2,3 +2,12 @@
 
 $a=1;
 adsadasd
+asdasdsa
+<datagrid>das
+da
+sdas
+<datagrid>asd
+asdasdsaas
+date_subdsa
+<datagrid><datagrid><datagrid>asd
+asd</datagrid></datagrid></datagrid></datagrid></datagrid>
